2124e5b1f9dab6f8f9982f8e0d69d942a200dbd5
Ritesh <riteshkapse1992@gmail.com>
2020-08-10 02:37:23 +0530

    user login api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -11,6 +11,11 @@ from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
 
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -114,5 +119,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
-    """commam to tell that it is tuple and not single item"""
+    """xcommam to tell that it is tuple and not single item"""
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+   """above created token are passed to request library of python and then used in header request to validate"""
+   """in javascript,fetch library"""
+   """both of them are http libraries"""
